6f3745b7a0dd32e6e9fca15fe3358cd4785867e1
BALAOASalikeith <balaoaskeith@gmail.com>
2024-09-13 23:57:06 +0800

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let name = "Allysa";
+let whatDoYouWannaBecomeInYourLife = "software developer";
+let gender = "Female";
+let twitterHandle = "@patotieee_ywxq";
+
+console.log("Allysa:", name)
+console.log("Software developer:", whatDoYouWannaBecomeInYourLIfe);
+console.log("Female:", gender);
+console.log("@patotieee_ywxq:", twitterHandle);
